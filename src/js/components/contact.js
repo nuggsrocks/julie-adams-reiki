60d8598ad4bbbafd4b65a0f98db4afe74fd3716b
mattfisher <nuggsrocks@yahoo.com>
2020-08-08 15:47:26 -0400

    added simple form setup

--- src/js/components/contact.js
+++ src/js/components/contact.js
@@ -5,13 +5,27 @@ import {splitAndFilterText} from '../functions';
 const Contact = () => {
 	return (
 		<div>
-			<h3>Services</h3>
-			{
-				splitAndFilterText(servicesTxt)
-					.map(value => <p key={value}>{value}</p>)
-			}
+			<article>
+				<h3>Services</h3>
+				{
+					splitAndFilterText(servicesTxt)
+						.map(value => <p key={value}>{value}</p>)
+				}
+			</article>
 
-			<h3>Contact</h3>
+			<article>
+				<h3>Contact</h3>
+				<form>
+					<label htmlFor='name'>Name:</label>
+					<input id='name' type='text' name='name'/>
+					<label htmlFor='email'>Email:</label>
+					<input id='email' type='text' name='email'/>
+					<label htmlFor='phone'>Phone Number:</label>
+					<input id='phone' type='text' name='phone'/>
+					<label htmlFor='comment'>Comments/Questions:</label>
+					<textarea id='comment' type='text' name='comment' rows='10' cols='20'></textarea>
+				</form>
+			</article>
 		</div>
 	)
 };
