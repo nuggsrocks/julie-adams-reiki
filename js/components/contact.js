eafc297d87a41218754f7a0bfc9ba6b3e75de693
mattfisher <nuggsrocks@yahoo.com>
2020-08-31 17:22:50 -0400

    added form validation and styling

--- js/components/contact.js
+++ js/components/contact.js
@@ -1,24 +1,24 @@
 import React from 'react';
 
 const Contact = () => {
+
 	return (
 		<div>
 			<h2>Contact</h2>
-
 			<form>
 				<section>
 					<label htmlFor='name'>Name</label>
-					<input type='text' name='name' id='name'/>
+					<input type='text' name='name' id='name' pattern='([A-z]|\s){1,24}'/>
 				</section>
 
 				<section>
 					<label htmlFor='email'>Email</label>
-					<input type='email' name='email' id='email'/>
+					<input type='email' name='email' id='email' pattern='[A-z]{1,30}@[A-z]{1,20}(.com|.net|.org|.edu|.io)'/>
 				</section>
 
 				<section>
 					<label htmlFor='phone'>Phone</label>
-					<input type='tel' name='phone' id='phone'/>
+					<input type='tel' name='phone' id='phone' pattern='1?-?\s?\(?[0-9]{3}\)?\s?-?[0-9]{3}-?[0-9]{4}'/>
 				</section>
 
 				<section>
@@ -30,9 +30,13 @@ const Contact = () => {
 					<label htmlFor='comment'>Comment</label>
 					<textarea id='comment'></textarea>
 				</section>
+
+				<section>
+					<input type='submit' value='Submit' id='submit'/>
+				</section>
 			</form>
 		</div>
-	);
+		);
 };
 
 export default Contact;
