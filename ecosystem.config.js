2acba80eb23b03b1bb542858e5501a35847c6cfd
mattfisher <nuggsrocks@yahoo.com>
2020-09-07 16:08:47 -0400

    added local network server start script

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,6 +5,11 @@ module.exports = {
 		script: './server.js',
 		watch: 'src',
 		env: {
+			HOST: 'localhost',
+			PORT: 8080
+		},
+		env_localServer: {
+			HOST: '192.168.1.15',
 			PORT: 8080
 		},
 		env_https: {
--- server.js
+++ server.js
@@ -3,10 +3,12 @@ const app = express();
 
 const port = process.env.PORT || 8080;
 
+const host = process.env.HOST || 'localhost';
+
 app.use(express.static(__dirname + '/public'));
 
 app.get('/*', (req, res) => {
 	res.sendFile(__dirname + '/public/index.html');
 });
 
-app.listen(port, '192.168.1.11', () => console.log('************'));
+app.listen(port, host, () => console.log('************'));
